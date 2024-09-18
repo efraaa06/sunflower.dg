4bb1e39f7eba8e92906ce83efb770aa12fef5474
Efra <your.email@example.com>
2024-09-17 21:13:23 -0600

    aks

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,7 @@ import { defineConfig } from "astro/config";
 
 import preact from "@astrojs/preact";
 
-export default {
-  build: {
-    rollupOptions: {
-      external: ['@astrojs/rss']
-    }
-  }
-}
+export default defineConfig({
+  site: "https://sunflowerdg.netlify.app/",
+  integrations: [preact()]
+});
