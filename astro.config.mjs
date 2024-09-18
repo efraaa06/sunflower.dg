5365e8968cbfc00d1073912acc4863ee96836142
Efra <your.email@example.com>
2024-09-17 21:10:16 -0600

    last

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,10 @@ import { defineConfig } from "astro/config";
 
 import preact from "@astrojs/preact";
 
-export default defineConfig({
-  site: "https://example.com",
-  integrations: [preact()]
-});
+export default {
+  build: {
+    rollupOptions: {
+      external: ['@astrojs/rss']
+    }
+  }
+}
